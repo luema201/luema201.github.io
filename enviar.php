d4e641432ec21a2959e5ec77949541f11d6986cc
luema201 <98621488+luema201@users.noreply.github.com>
2024-02-06 19:33:27 -0300

    Initial
    a

---
+++ enviar.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recoge los datos del formulario
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    // Dirección de correo a la que se enviará el formulario
+    $destinatario = "luemadiaz1@gmail.com";
+
+    // Asunto del correo
+    $asunto = "Respuesta";
+
+    // Cuerpo del mensaje
+    $cuerpoMensaje = "Nombre: $nombre\n";
+    $cuerpoMensaje .= "Email: $email\n";
+    $cuerpoMensaje .= "Mensaje:\n$mensaje";
+
+    // Cabeceras del correo
+    $cabeceras = "From: $email\r\nReply-To: $email";
+
+    // Intenta enviar el correo
+    if (mail($destinatario, $asunto, $cuerpoMensaje, $cabeceras)) {
+        echo "¡El formulario ha sido enviado con éxito!";
+    } else {
+        echo "Hubo un problema al enviar el formulario. Por favor, inténtalo de nuevo.";
+    }
+}
+?>
